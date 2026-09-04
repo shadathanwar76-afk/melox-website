b41e1af55ca68c706438be8f7ec95895886f4813
shadathanwar76-afk <shadathanwar76@gmail.com>
2026-09-04 19:44:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -31,7 +31,12 @@ const storage = multer.diskStorage({
     }
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 50 * 1024 * 1024
+    }
+});
 
 // Upload file
 app.post("/upload", upload.single("file"), (req, res) => {
